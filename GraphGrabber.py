c122a9ea897e9f5efd4974cd97133032920da5a7
Laurapaws <40493346+Laurapaws@users.noreply.github.com>
2022-05-03 12:15:36 +0100

    Added VT-15 functionality

--- GraphGrabber.py
+++ GraphGrabber.py
@@ -14,7 +14,7 @@ from io import BytesIO
 posDict = {
     # Define coordinates for positioning. Format is test name then test type. e.g. a VT-07 test with a mediumwave plot
     # Tuple Order: Left , Top, Width, Height
-    # VT07
+    # VT-07
     'VT07MW' : (Pt(1), Pt(70), Pt(233), Pt(176)),
     'VT07FM1' : (Pt(239), Pt(70), Pt(233), Pt(176)),
     'VT07FM2' : (Pt(477), Pt(70), Pt(233), Pt(176)),
@@ -22,23 +22,36 @@ posDict = {
     'VT07DAB1RMS' : (Pt(180), Pt(275), Pt(175), Pt(130)),
     'VT07DAB2AV' : (Pt(360), Pt(275), Pt(175), Pt(130)),
     'VT07DAB2RMS' : (Pt(540), Pt(275), Pt(175), Pt(130)),
-    # VT01 3 Metre
+    # VT-01 3 Metre
     'VT01ThreeVertical' : (Pt(1), Pt(85), Pt(358), Pt(270)),
     'VT01ThreeHorizontal' : (Pt(360), Pt(85), Pt(358), Pt(270)),
-    # VT12 Single Phase
+    # VT-12 Single Phase
     'VT12SingleL1' : (Pt(1), Pt(85), Pt(358), Pt(270)),
     'VT12SingleN' : (Pt(360), Pt(85), Pt(358), Pt(270)),
-    # VT12 Three Phase
+    # VT-12 Three Phase
     'VT12TripleL1' : (Pt(1), Pt(65), Pt(221), Pt(167)),
     'VT12TripleL2' : (Pt(222), Pt(65), Pt(221), Pt(167)),
     'VT12TripleL3' : (Pt(1), Pt(240), Pt(221), Pt(167)),
     'VT12TripleN' : (Pt(222), Pt(240), Pt(221), Pt(167)),
+    # VT-15 Electric
+    'VT15E16' : (Pt(1), Pt(85), Pt(233), Pt(176)),
+    'VT15E40' : (Pt(239), Pt(85), Pt(233), Pt(176)),
+    'VT15E70' : (Pt(477), Pt(85), Pt(233), Pt(176)),
+    # VT-15 Magnetic Radial and Transverse
+    'VT15HR16' : (Pt(1), Pt(65), Pt(221), Pt(167)),
+    'VT15HR40' : (Pt(239), Pt(65), Pt(221), Pt(167)),
+    'VT15HR70' : (Pt(477), Pt(65), Pt(221), Pt(167)),
+    'VT15HT16' : (Pt(1), Pt(235), Pt(221), Pt(167)),
+    'VT15HT40' : (Pt(239), Pt(235), Pt(221), Pt(167)),
+    'VT15HT70' : (Pt(477), Pt(235), Pt(221), Pt(167)),
 }
 
 cropDict = {
     # Define coordinates for cropping. Old refers to the old style PDFs we use that crop in two static positions
+    # Left Start, Top Start, Left End, Top End
     'upperOld' : ((130, 138, 1000, 800)),
-    'lowerOld' : ((130, 820, 1000, 1482))
+    'lowerOld' : ((130, 820, 1000, 1482)),
+    'upperOldMagnetic' : ((130, 270, 1000, 932))
 }
 
 nameDict = {
@@ -48,7 +61,8 @@ nameDict = {
     'VT07' : 'VT-07 On-Board Emissions',
     'VT12Single' : 'VT-12 Conducted Emissions (Single Phase)',
     'VT12Triple' : 'VT-12 Conducted Emissions (Three Phase)',
-    'VT15' : 'VT-15 E & H Fields'
+    'VT15Electric' : 'VT-15 Electric Fields',
+    'VT15Magnetic' : 'VT-15 Magnetic Fields'
 }
 
 # Defining global variables
@@ -118,6 +132,7 @@ def initialisePowerPoint(emptyDeckName, newDeckName):
     print('Created new PowerPoint: ' + newDeckName)
 
 
+
 def VT07(PDFName, folderName, slideNumber, deckName):
     extractImages(PDFName, folderName)
     cropGraph(extractedImages[1], cropDict['upperOld'], 'MW')
@@ -138,7 +153,6 @@ def VT07(PDFName, folderName, slideNumber, deckName):
     extractedImages.clear()
     croppedImages.clear()
     print('Finished VT-07 for ' + PDFName)
-
 
 def VT01Three(PDFName, folderName, slideNumber, deckName):
     extractImages(PDFName, folderName)
@@ -177,6 +191,37 @@ def VT12Triple(PDFName, folderName, slideNumber, deckName):
     croppedImages.clear()
     print('Finished VT-12 Three Phase for ' + PDFName)
 
+def VT15Electric(PDFName, folderName, slideNumber, deckName):
+    extractImages(PDFName, folderName)
+    cropGraph(extractedImages[1], cropDict['upperOld'], 'VT15E16')
+    cropGraph(extractedImages[1], cropDict['lowerOld'], 'VT15E40')
+    cropGraph(extractedImages[2], cropDict['upperOld'], 'VT15E70')
+    deckName = deckName + '.pptx'
+    insertImage(deckName, deckName, croppedImages[0], posDict['VT15E16'], slideNumber)
+    insertImage(deckName, deckName, croppedImages[1], posDict['VT15E40'], slideNumber)
+    insertImage(deckName, deckName, croppedImages[2], posDict['VT15E70'], slideNumber)
+    extractedImages.clear()
+    croppedImages.clear()
+    print('Finished VT-15 Electric Field for ' + PDFName)
+
+def VT15Magnetic(PDFName, folderName, slideNumber, deckName):
+    extractImages(PDFName, folderName)
+    cropGraph(extractedImages[1], cropDict['upperOldMagnetic'], 'VT15HR16')
+    cropGraph(extractedImages[2], cropDict['upperOld'], 'VT15HR40')
+    cropGraph(extractedImages[2], cropDict['lowerOld'], 'VT15HR70')
+    cropGraph(extractedImages[3], cropDict['upperOld'], 'VT15HT16')
+    cropGraph(extractedImages[3], cropDict['lowerOld'], 'VT15HT40')
+    cropGraph(extractedImages[4], cropDict['upperOld'], 'VT15HT70')
+    deckName = deckName + '.pptx'
+    insertImage(deckName, deckName, croppedImages[0], posDict['VT15HR16'], slideNumber)
+    insertImage(deckName, deckName, croppedImages[1], posDict['VT15HR40'], slideNumber)
+    insertImage(deckName, deckName, croppedImages[2], posDict['VT15HR70'], slideNumber)
+    insertImage(deckName, deckName, croppedImages[3], posDict['VT15HT16'], slideNumber)
+    insertImage(deckName, deckName, croppedImages[4], posDict['VT15HT40'], slideNumber)
+    insertImage(deckName, deckName, croppedImages[5], posDict['VT15HT70'], slideNumber)
+    extractedImages.clear()
+    croppedImages.clear()
+    print('Finished VT-15 Electric Field for ' + PDFName)
 
 def setSlideCounter(num):
     global slideCounter
@@ -208,12 +253,10 @@ initialisePowerPoint('emptyDeck', 'newDeck')
 setSlideCounter(0)
 
 #loopFolder('VT-01 3m','newDeck', VT01Three)
-
 #loopFolder('VT-07','newDeck', VT07)
-
 #loopFolder('VT-12 Single Phase', 'newDeck', VT12Single)
-
-loopFolder('VT-12 Three Phase', 'newDeck', VT12Triple)
-
+#loopFolder('VT-12 Three Phase', 'newDeck', VT12Triple)
+#loopFolder('VT-15 Electric', 'newDeck', VT15Electric)
+loopFolder('VT-15 Magnetic', 'newDeck', VT15Magnetic)
 
 print('Finished all jobs...')
